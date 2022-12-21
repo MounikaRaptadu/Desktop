1f717b46ace3ccd11347162ca34636a59d9c0840
MounikaRaptadu <mounika15042001@gmail.com>
2022-12-21 11:59:43 +0530

    ok

---
+++ js 3iterative/one.js
@@ -0,0 +1,18 @@
+//print 1,2,3,4,5
+/*
+intilization:leti=1
+condition:i<=5
+increment/decrement:i++
+*/
+for(i=1;i<=5;i++){
+    console.log(i)
+}
+console.log("*****")
+i=1
+while(i>=5){
+    console.log(i)
+    i++
+}
+console.log("*****")
+i++
+
