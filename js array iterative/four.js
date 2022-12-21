175b4576d8aae115c1f5f078b1904b0bcdc42ac5
MounikaRaptadu <mounika15042001@gmail.com>
2022-12-21 13:05:34 +0530

    ok

---
+++ js array iterative/four.js
@@ -0,0 +1,9 @@
+let enames=["mounika","chaya","pooju","meena","sravanthi"]
+let ids=[101,102,103,104,105]
+//index     0            1         2     3        4
+console.log(ids)
+//print all enames
+let(i<=4)
+    while(i<=4){enames[i]}
+    i++
+
