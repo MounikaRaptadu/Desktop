725e31e589e1ba31b1866f849a0c5bf907d1c622
MounikaRaptadu <mounika15042001@gmail.com>
2022-12-21 12:47:24 +0530

    ok

---
+++ js array iterative/two.js
@@ -0,0 +1,12 @@
+let enames=["mounika","meena","chaya","sravanthi","pooju"]
+let ids=[101,102,103,104,105]
+//index   0    1   2  3   4
+console.log(enames)
+//print all ename
+console.log(enames[0])
+console.log(enames[1])
+console.log(enames[2])
+console.log(enames[3])
+console.log(enames[4])
+
+
